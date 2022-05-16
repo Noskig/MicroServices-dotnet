c9b00a6e42424bd9ad73c7443cfad9abd62c60dc
Noskig <48559023+Noskig@users.noreply.github.com>
2022-05-16 16:31:54 +0200

    Add project files.

---
+++ Data/IPlatfromRepo.cs
@@ -0,0 +1,13 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public interface IPlatfromRepo
+    {
+        bool SaveChanges();
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatforById(int id);
+        void CreatePlatform(Platform plat);
+
+    }
+}
